5c49ddaf26b3f7113effdb0e1f2c965fe9354cb2
Vasily Vasinov <des.elyon@gmail.com>
2020-10-30 14:26:43 -0600

    Update file mounts

---
+++ files/ray_worker/startup.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e
+
+# Make sure we wait enough time for the head node to start a Redis server. 5 seconds should be more
+# than plenty.
+sleep 5
+
+start.sh ray start \
+  --address {{ engines.ray_jupyter.vars.ray_head_host }}:{{ engines.ray_jupyter.vars.ray_head_port }} \
+  --node-ip-address $(hostname -I) \
+  --block \
+  --object-manager-port {{ engines.ray_worker.vars.ray_object_manager_port }} \
+  --node-manager-port {{ engines.ray_worker.vars.ray_node_manager_port }} \
+  --num-cpus {{ engines.ray_worker.deployment.hardware.cpu.vcpus }} \
+  --num-gpus {{ engines.ray_worker.deployment.hardware.gpus }} \
+  --min-worker-port 11000 \
+  --max-worker-port 12000 \
+  --verbose
