60c4a2d1658283f841a9490e5b62c8ef94e4b788
Vasily Vasinov <des.elyon@gmail.com>
2020-11-10 20:09:16 -0700

    Updating app.

--- files/ray_jupyter/startup.sh
+++ files/ray_jupyter/startup.sh
@@ -5,12 +5,12 @@ set -e
 start.sh ray start \
   --head \
   --node-ip-address $(hostname -I) \
-  --port {{ engines.ray_jupyter.vars.ray_head_port }} \
-  --object-manager-port {{ engines.ray_jupyter.vars.ray_object_manager_port }} \
-  --node-manager-port {{ engines.ray_jupyter.vars.ray_node_manager_port }} \
-  --gcs-server-port {{ engines.ray_jupyter.vars.gcs_server_port }} \
+  --port {{ engines.ray_jupyter.ports.ray_head_port.value }} \
+  --object-manager-port {{ engines.ray_jupyter.ports.ray_object_manager_port.value }} \
+  --node-manager-port {{ engines.ray_jupyter.ports.ray_node_manager_port.value }} \
+  --gcs-server-port {{ engines.ray_jupyter.ports.gcs_server_port.value }} \
   --include-dashboard true \
-  --dashboard-port {{ engines.ray_jupyter.vars.ray_dashboard_port }} \
+  --dashboard-port {{ engines.ray_jupyter.ports.ray_dashboard_port.value }} \
   --dashboard-host 0.0.0.0 \
   --num-cpus {{ engines.ray_jupyter.deployment.hardware.cpu.vcpus }} \
   --num-gpus {{ engines.ray_jupyter.deployment.hardware.gpus }} \
@@ -20,5 +20,5 @@ start.sh ray start \
 
 start-notebook.sh \
   --NotebookApp.token="{{ engines.ray_jupyter.vars.jupyter_token }}" \
-  --NotebookApp.port={{ engines.ray_jupyter.vars.jupyter_port }} \
+  --NotebookApp.port={{ engines.ray_jupyter.ports.jupyter_port.value }} \
   --no-browser
